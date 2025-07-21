66191e47cfc7682d38e5cf5a9fffd6e7e813832e
basem-samir <basemsamir.dev@gmail.com>
2025-07-22 02:49:54 +0300

    display list of notes

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,10 +1,10 @@
+import NotesList from "./NotesList";
+
 export default function MainContent() {
   return (
-    <div>
-      <h1 className="text-2xl font-bold">Notes</h1>
-      {/* filters */}
-      {/* search */}
-      {/* note list */}
+    <div className="py-10 px-5  overflow-auto max-h-full">
+      <h1 className="text-3xl font-bold text-gray-800 mb-4">Notes</h1>
+      <NotesList />
     </div>
   );
 }
---
+++ src/components/NotesList.jsx
@@ -0,0 +1,37 @@
+import { useSelector } from "react-redux";
+
+export default function NotesList() {
+  // get notes from redux
+  const notes = useSelector((state) => state.notes.notes);
+
+  return (
+    <div className="space-y-4 mt-6">
+      {notes.map((note) => (
+        <div
+          key={note.id}
+          className="note bg-white p-4 rounded-xl shadow-md flex flex-col gap-3"
+        >
+          <div className="flex justify-between items-start">
+            <h3 className="text-lg font-semibold text-gray-800 break-words">
+              {note.text}
+            </h3>
+
+            <div className="flex gap-2">
+              <button className="text-xl">✒️</button>
+
+              <button className="text-xl">❌</button>
+            </div>
+          </div>
+
+          <div className="flex justify-between text-sm text-gray-500">
+            <span className="text-[#555] text-sm rounded">{note.category}</span>
+
+            <span className="text-[#555] text-sm rounded">
+              {note.createdAt}
+            </span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
